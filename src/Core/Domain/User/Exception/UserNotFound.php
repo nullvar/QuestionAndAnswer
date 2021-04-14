2d32a36f5060774abffa61fe26a83ab9ef9a29da
Anton Karamnov <nullvar@yandex.ru>
2021-04-14 11:07:42 +0300

    A bug when DomainException not exists fixed

--- src/Core/Domain/User/Exception/UserNotFound.php
+++ src/Core/Domain/User/Exception/UserNotFound.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Core\Domain\User\Exception;
 
-use App\Core\Domain\DomainException;
+use App\Core\Domain\Exception\DomainException;
 
 final class UserNotFound extends DomainException
 {
