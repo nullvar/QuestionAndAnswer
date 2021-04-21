661c6223a704590edcca13ac34341604f353c46a
Anton Karamnov <nullvar@yandex.ru>
2021-04-21 17:16:10 +0300

    Refactored

--- src/Core/Services/Persistence/TransactionHandler.php
+++ src/Core/Services/Persistence/TransactionHandler.php
@@ -7,10 +7,7 @@ namespace App\Core\Services\Persistence;
 interface TransactionHandler
 {
     public function begin(): void;
-
     public function commit(): void;
-
     public function rollback(): void;
-
     public function clear(): void;
 }
