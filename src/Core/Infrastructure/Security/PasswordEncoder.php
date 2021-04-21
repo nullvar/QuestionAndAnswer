5e80ce9a4c96374812488607432e3b8c47d8da97
Anton Karamnov <nullvar@yandex.ru>
2021-04-21 22:03:01 +0300

    Refactored

--- src/Core/Infrastructure/Security/PasswordEncoder.php
+++ src/Core/Infrastructure/Security/PasswordEncoder.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Core\Infrastructure\Persistence\User;
+namespace App\Core\Infrastructure\Security;
 
 use App\Core\Domain\User\Contract\PasswordEncoder as PasswordEncoderInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
